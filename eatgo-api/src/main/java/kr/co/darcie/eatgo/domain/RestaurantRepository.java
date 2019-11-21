80865c2302952db710d5bb060bbbda99444ede23
luckysoo3516 <sumin328030@gmail.com>
2019-11-21 15:04:39 +0900

    Create Restaurant Repository
    Sepreate collection of restuarant from controller.
    Find all of restaurnats.
    Handle Collection of restuarants.

---
+++ eatgo-api/src/main/java/kr/co/darcie/eatgo/domain/RestaurantRepository.java
@@ -0,0 +1,14 @@
+package kr.co.darcie.eatgo.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantRepository {
+    public List<Restaurant> findAll() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+
+        return restaurants;
+    }
+}
