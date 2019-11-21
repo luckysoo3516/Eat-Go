1028526e8807c6e42003a4b2f7ed1cc4d0e10954
luckysoo3516 <sumin328030@gmail.com>
2019-11-21 14:51:25 +0900

    Add initial version of Eat-Go
    Create domain model and controller.
    Create tests of each methods.

---
+++ eatgo-api/src/main/java/kr/co/darcie/eatgo/domain/Restaurant.java
@@ -0,0 +1,30 @@
+package kr.co.darcie.eatgo.domain;
+
+public class Restaurant {
+    private final String name;
+    private final String address;
+    private final Long id;
+
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInformation() {
+        return name + " in " + address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+}
