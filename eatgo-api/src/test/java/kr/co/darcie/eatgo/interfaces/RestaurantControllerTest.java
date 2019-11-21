29c3c25d0c3d8fd000d3db6c66fd009c53ea198b
luckysoo3516 <sumin328030@gmail.com>
2019-11-21 23:45:05 +0900

    Create mock objects
    Create mock repository objects to test service.

--- eatgo-api/src/test/java/kr/co/darcie/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/darcie/eatgo/interfaces/RestaurantControllerTest.java
@@ -11,7 +11,11 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,25 +33,40 @@ public class RestaurantControllerTest {
 
     @Test
     public void list() throws Exception {
+
+        List<Restaurant> restuarants = new ArrayList<>();
+        restuarants.add(new Restaurant(1004L, "JOCKER House", "Seoul"));
+
+        given(restaurantService.getRestaurants()).willReturn(restuarants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":1004"))
                 )
                 .andExpect(content().string(
-                        containsString("\"name\":\"Bob zip\""))
+                        containsString("\"name\":\"JOCKER House\""))
                 );
 
     }
 
     @Test
     public void detail() throws Exception {
+        Restaurant restaurant1 = new Restaurant(1004L, "JOCKER House", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber Food", "Seoul");
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
+
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":1004")))
                 .andExpect(content().string(
-                        containsString("\"name\":\"Bob zip\"")))
+                        containsString("\"name\":\"JOCKER House\"")))
                 .andExpect(content().string(
                 containsString("Kimchi"))
         );
